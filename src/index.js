031b0237696c21b33b390927043a5789d757897a
joanaferreira80 <joanaferreira.br@gmail.com>
2025-07-29 16:30:22 +0100

    update UX

--- src/index.js
+++ src/index.js
@@ -19,6 +19,13 @@ let apikey="t61c0cb24c8co6107fad0a63023aa3ea";
 let prompt = `Write a portuguese poem about ${InstructionsElement.value}`;
 let context = "you are a AI assistant good to write simple portuguese poems in Portuguese. Write a 4 line poem and separate each line with <br>. Sign the poem with a <strong>She Codes AI</strong>";
 let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
+
+ let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating"> Generating a PT poem about ${instructionsInput.value}</div>`;
+
+
+
 axios.get(apiUrl).then(displayPoem);
 
 }
